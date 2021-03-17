6dd0e95b2e3297a472b37e9747a9cf237610903f
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-03-17 13:44:31 -0300

    :wrench: chore: setup to handles calls to api

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import {App} from './App';
 
 createServer({
+  models: {
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance mobile',
+          type: 'income',
+          category: 'Dev',
+          amount: 5000,
+          createdAt: new Date('2021-02-11 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'outcome',
+          category: 'venda',
+          amount: 2000,
+          createdAt: new Date('2021-02-13 09:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transactions 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        }
-      ]
+      return this.schema.all('transactions');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody).transactions;
+
+      return schema.create('transactions', data)
     })
   }
 });
