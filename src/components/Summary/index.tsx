2e783a7d7b28b513e689d2f70cb215a15b3f30b6
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-03-17 17:43:18 -0300

    :sparkles: feat: connect with context transaction and format card values

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,9 +2,38 @@ import arrowUpImg from '../../assets/arrow-up.svg';
 import arrowDownImg from '../../assets/arrow-down.svg';
 import totalImg from '../../assets/total.svg';
 
+import { useContext } from 'react';
+import { TransactionsContext } from '../../context';
+
 import * as S from './styles';
+import { formatValue } from '../../utils/format';
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const { income, outcome, total } = transactions.reduce((accumulator, transaction) => {
+    if (transaction.type === "income") {
+      accumulator.income += transaction.amount;
+      accumulator.total += transaction.amount;
+    } else {
+      accumulator.outcome += transaction.amount;
+      accumulator.total -= transaction.amount;
+    }
+
+    return accumulator;
+    
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  });
+
+  const summaryFormatted = {
+    income: formatValue(income),
+    outcome: formatValue(outcome),
+    total: formatValue(total),
+  }
+
   return (
     <S.Container>
       <S.Card>
@@ -13,7 +42,7 @@ export function Summary() {
 
           <img src={arrowUpImg} alt="Arrow up"/>
         </header>
-        <strong>R$ <span>28.487</span>,00</strong>
+        <strong>{summaryFormatted.income}</strong>
       </S.Card>
 
       <S.Card>
@@ -22,7 +51,7 @@ export function Summary() {
 
           <img src={arrowDownImg} alt="Arrow down"/>
         </header>
-        <strong>R$ <span>2.487</span>,00</strong>
+        <strong>{summaryFormatted.outcome}</strong>
       </S.Card>
 
       <S.Card className="hightlight-background">
@@ -31,7 +60,7 @@ export function Summary() {
 
           <img src={totalImg} alt="Total"/>
         </header>
-        <strong>R$ <span>26.487</span>,00</strong>
+        <strong>{summaryFormatted.total}</strong>
       </S.Card>
     </S.Container>
   );
